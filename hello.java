e36382dd9cc954992180063c3afd2e45eceacfba
ASHISH KUNDU <ak975732@gmail.com>
2024-01-11 16:14:31 +0000

    changes for child

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello my name is ashish kundu and this command is running from child branch”);
+System.out.println(“hello my name is ashish kundu and this command is running from child branc = this is now been committedh”);
 }
 }
 
